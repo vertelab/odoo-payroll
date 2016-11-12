db6e3f01bc7405e756f30c8484563026ea651a40
Anders Wallenquist <anders.wallenquist@vertel.se>
2016-11-12 18:35:51 +0100

    hr_payroll_employeefund: account-line

--- hr_payroll_employeefund/hr_payroll.py
+++ hr_payroll_employeefund/hr_payroll.py
@@ -29,12 +29,29 @@ class hr_contract(models.Model):
 
     employee_fund      = fields.Many2one(string="Employee Fund", digits_compute=dp.get_precision('Payroll'),comodel_name='account.analytic.account',help="Use this account together with marked salary rule" )
 
-
-
 class hr_salary_rule(models.Model):
     _inherit = 'hr.salary.rule'
     
     use_employee_fund = fields.Boolean(string="Use for employee fund",default=False)
 
 
+class hr_payslip(models.Model):
+	_inherit = 'hr.payslip'
+	
+	@api.one
+    def process_sheet(self):
+		for line in self.details_by_salary_rule_category:
+			if line.salary_rule_id.use_employee_fund and self.employee_id.contract_id.employee_fund:
+				request.env['account.analytic.line'].create({
+					'name': 'Payslip %s' % self.name,
+					'date': fields.Date.today(),
+					'account_id': self.employee_id.contract_id.employee_fund,
+					'unit_amount': 1,
+					'amount': line.amount,  
+					'general_account_id': request.env['account.account'].search([('code', '=', '3000')]).id,
+					'user_id': employee.user_id.id if employee.user_id else None,
+					'journal_id': request.env.ref('hr_timesheet.analytic_journal').id,
+				})
+		return super(hr_payslip, self).process_sheet()
+		
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
