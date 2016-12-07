5da87b726b318df6f9e455d7ea1ea9dba8c0cd76
Haojun Zou <apollo_zhj@msn.com>
2016-12-07 17:59:42 +0100

    hr_payroll_employeefund: new general method

--- hr_payroll_employeefund/hr_payroll.py
+++ hr_payroll_employeefund/hr_payroll.py
@@ -30,35 +30,35 @@ class hr_contract(models.Model):
     employee_fund      = fields.Many2one(string="Employee Fund",comodel_name='account.analytic.account',help="Use this account together with marked salary rule" )
     employee_fund_balance = fields.Float(string='Balance',digits_compute=dp.get_precision('Payroll'),related='employee_fund.balance')
     employee_fund_name = fields.Char(string='Name',related='employee_fund.name')
-    
+
 
 class hr_salary_rule(models.Model):
     _inherit = 'hr.salary.rule'
-    
+
     use_employee_fund = fields.Boolean(string="Use for employee fund",default=False)
 
 class hr_payslip(models.Model):
     _inherit = 'hr.payslip'
 
-    @api.one
+    @api.multi
     def get_employeefund_addition(self):
         return sum(self.env['account.analytic.line'].search([('account_id','=',self.contract_id.employee_fund.id),('date','>=',self.date_from),('date','<=',self.date_to),('amount','>',0.0)]).mapped('amount'))
-    
+
 
     @api.one
     def process_sheet(self):
         for line in self.details_by_salary_rule_category:
             if line.salary_rule_id.use_employee_fund and self.employee_id.contract_id.employee_fund:
-			    self.env['account.analytic.line'].create({
-					'name': 'Payslip %s' % self.name,
-					'date': fields.Date.today(),
-					'account_id': self.employee_id.contract_id.employee_fund.id,
-					'unit_amount': 1.0,
-					'amount': line.amount,  
-					'general_account_id': self.env['account.account'].search([('code', 'in', ['7690','7610','7600','7699'])])[0].id,
-					'user_id': self.employee_id.user_id.id if self.employee_id.user_id else None,
-					'journal_id': self.env.ref('account.exp').id,
-				})
+                self.env['account.analytic.line'].create({
+                    'name': 'Payslip %s' % self.name,
+                    'date': fields.Date.today(),
+                    'account_id': self.employee_id.contract_id.employee_fund.id,
+                    'unit_amount': 1.0,
+                    'amount': line.amount,
+                    'general_account_id': self.env['account.account'].search([('code', 'in', ['7690','7610','7600','7699'])])[0].id,
+                    'user_id': self.employee_id.user_id.id if self.employee_id.user_id else None,
+                    'journal_id': self.env.ref('account.exp').id,
+                })
         return super(hr_payslip, self).process_sheet()
-		
+
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
