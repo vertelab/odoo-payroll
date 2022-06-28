6aa3f0a5d2d398167c59162726649ec4b0ac9577
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-06-28 16:36:55 +0200

    Update __manifest__.py
    fixing with the manifest

--- hr_payroll_employeefund_expenses/__manifest__.py
+++ hr_payroll_employeefund_expenses/__manifest__.py
@@ -1,43 +1,47 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-# OpenERP, Open Source Management Solution, third party addon
-# Copyright (C) 2016- Vertel AB (<http://vertel.se>).
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2022- Vertel AB (<https://vertel.se>).
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU Affero General Public License for more details.
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
 #
-# You should have received a copy of the GNU Affero General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
+
 {
-    'name': "hr_payroll_employeefund_expenses",
-
-    'summary': """
-        Extends hr_payroll_employeefund with an extra employeefund""",
-
+    'name': 'Payroll: HR Payroll Employee Fund Expenses',
+    'version': '14.0.0.2.0',
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'summary': 'Extends hr_payroll_employeefund with an extra employeefund',
+    'category': 'Productivity',
     'description': """
         Adds an employeefund-view to the Employee Contract and the Expense viewform.
         
         0.2 - Added a button to allow edit of cost/revenue from expense
     """,
-
-    'author': "Vertel AB",
-    'website': "https://www.vertel.se",
+    #'sequence': '1',
+    'author': 'Vertel AB',
+    'website': 'https://vertel.se/apps/odoo-payroll/hr_payroll_employeefund_expenses',
+    'images': ['/static/description/banner.png'], # 560x280 px.
+    'license': 'AGPL-3',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-payroll',
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'hr',
-    'version': '0.2',
 
     # any module necessary for this one to work correctly
     'depends': ['hr_payroll_employeefund','hr_expense'],
