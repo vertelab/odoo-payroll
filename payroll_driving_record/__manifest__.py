4ef574ee202d4bc5a088dbf08b7bd49734ea9fc3
Björn Hayer <bjorn.hayer@vertel.se>
2022-09-02 07:22:44 +0000

    Rectified 'Reckord' to 'Record'

--- payroll_driving_record/__manifest__.py
+++ payroll_driving_record/__manifest__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 
 {
-    'name': 'Payroll: Payroll Driving Reckord',
+    'name': 'Payroll: Payroll Driving Record',
     'version': '14.0.0.0.0',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': """
