caa45d68fa57bd795dbfd23f28e1acf725e719f5
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2023-02-22 11:35:17 +0100

    Update __manifest__.py
    cleaning in manifest

--- payroll_driving_record_crm_lead/__manifest__.py
+++ payroll_driving_record_crm_lead/__manifest__.py
@@ -23,11 +23,10 @@
     'name': 'Payroll: Driving Record: CRM',
     'version': '14.0.0.0.0',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
-    'summary': """
-        A glue module which gives smartbutton functionality to CRM for Driving Records""",
+    'summary': 'A glue module which gives smartbutton functionality to CRM for Driving Records',
     'category': 'Productivity',
     'description': """
-        A glue module which gives smartbutton functionality to CRM for Driving Records.
+    A glue module which gives smartbutton functionality to CRM for Driving Records.
     """,
     #'sequence': '1',
     'author': 'Vertel AB',
