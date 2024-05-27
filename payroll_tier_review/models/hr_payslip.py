89dce1f0248269d4ffb43753cde122568698cb03
iammiracle <miracleaayodele@gmail.com>
2024-05-27 11:59:56 +0200

    added fields that are allowed to write to

--- payroll_tier_review/models/hr_payslip.py
+++ payroll_tier_review/models/hr_payslip.py
@@ -4,10 +4,20 @@ from odoo import models, fields, api, _
 class Payslip(models.Model):
     _name = "hr.payslip"
     _inherit = ["hr.payslip", "tier.validation"]
-    _state_from = ["draft"]
+    _state_from = ["verify"]
     _state_to = ["done"]
 
     _tier_validation_manual_config = False
+
+    @api.depends("need_validation")
+    def _compute_hide_post_button(self):
+        result = super()._compute_hide_post_button()
+        for this in self:
+            this.hide_post_button |= this.need_validation
+        return result
+
+    def _get_under_validation_exceptions(self):
+        return super()._get_under_validation_exceptions() + ["move_id", "compute_date", "number", "line_ids", "state"]
 
     def _get_to_validate_message_name(self):
         name = super(Payslip, self)._get_to_validate_message_name()
