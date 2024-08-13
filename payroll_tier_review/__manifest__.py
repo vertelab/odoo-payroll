a3cdd2082b78836732bd274d2b51e8bd373ad2d1
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2024-08-13 16:57:08 +0200

    Update __manifest__.py

--- payroll_tier_review/__manifest__.py
+++ payroll_tier_review/__manifest__.py
@@ -1,6 +1,27 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2024- Vertel AB (<https://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
 {
     'name': 'Payroll: Tier Review',
-    'version': '14.0.0.0.0',
+    'version': '0.1',
     'summary': """
         A glue module which adds tier validation to payslip""",
     'category': 'Productivity',
@@ -15,7 +36,6 @@
     'maintainer': 'Vertel AB',
     'repository': 'https://github.com/vertelab/odoo-payroll',
     'category': 'Employee',
-    'version': '0.1',
     'depends': ['base_tier_validation', 'payroll'],
     'data': [
         'views/hr_payslip_view.xml'
